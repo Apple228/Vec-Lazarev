d565a95a0b300b8cf66e3ffeeba52c631b4d9115
Lazarev Alex <alekse.lazarev.01@gmail.com>
2020-05-24 13:37:08 +0300

    Signed-off-by: Lazarev Alex <alekse.lazarev.01@gmail.com>

--- source/class.h
+++ source/class.h
@@ -1,226 +1,208 @@
 #pragma once
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
-inline int min(int a, int b)
-{
-	return (a <= b) ? a : b;
-}
 
-inline int min(int, int);
+class row; // Класс "строка"
+class matrix; // Класс "матрица"
 
-
-class Vec
+class row // Класс "строка"
 {
 public:
-	double* arr;
-	int size;
-	Vec(int len)
-	{
-		size = len;
-		arr = new double[len];
-	}
-
-	friend Vec operator + (Vec const& a, Vec const& b)
-	{
-		Vec result = Vec(a.size);
-		for (int i = 0; i < a.size; i++)
-		{
-			result.arr[i] = a.arr[i] + b.arr[i];
-		}
-		return result;
-	}
-
-
-	void print()
-	{
-		for (int i = 0; i < size; i++)
-		{
-			std::cout << arr[i] << " ";
-		}
-		std::cout << "\n";
-	}
-
-	friend std::ostream& operator << (std::ostream& out, const Vec& c);
-
-
-};
-std::ostream& operator << (std::ostream& out, const Vec& c)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		out << c.arr[i] << " ";
-	}
-	out << "\n";
-
-	return out;
-
-}
-
-class Matrix
-{
+	friend class matrix;
+	int& operator [] (int j); // Оператор доступа к элементу
+	int get_sz() { return sz; }
+	int* get_massiv() { return p; }
 private:
-	long int** Matr;
-	long int line;
-	long int collumn;
-
-	void Create()
-	{
-		Matr = new long int* [line];
-		for (int z = 0; z < line; z++)
-			Matr[z] = new long int[collumn];
-	}
-
-public:
-	// constructors and destructor
-	Matrix() : line(5), collumn(5) { Create(); } //создаст 5 на 5
-	Matrix(long int i) : line(i), collumn(i) { Create(); } //создаст квадратную матрицу 
-	Matrix(long int i, long int j) : line(i), collumn(j) { Create(); } //нужного размера
-	~Matrix()
-	{
-		for (int z = 0; z < line; z++)
-			delete[] Matr[z];
-		delete[] Matr;
-	}
-
-	// methods
-	long int &Element(long int i, long int j)
-	{
-		if (i < line && j < collumn)
-			return Matr[i][j];
-		else
-			cout << "Error: 1";
-	}
-	
-
-	
-	void MultiplyBy(long int x) //умножение матрицы на какое-то число
-	{
-		for (int i = 0; i < line; i++)
-			for (int j = 0; j < collumn; j++)
-				Matr[i][j] *= x;
-	}
-	//вывод на экран 
-	void Display()
-	{
-		for (int i = 0; i < line; i++)
-		{
-			for (int j = 0; j < collumn; j++)
-			{
-				cout.width(4);//промежуток между числами
-				cout << Matr[i][j];
-			}
-			cout << endl;
-		}
-	}
-	// Конструктор копирования
-	Matrix(const Matrix& _M)
-	{
-		// Создается новый объект для которого виделяется память
-		// Копирование данных *this <= _M
-		line = _M.line;
-		collumn = _M.collumn;
-
-		Matr = new long int* [line];
-		for (int z = 0; z < line; z++)
-			Matr[z] = new long int[collumn];
-		
-		// заполнить значениями
-		for (int i = 0; i < line; i++)
-			for (int j = 0; j < collumn; j++)
-				Matr[i][j] = _M.Matr[i][j];
-	}
-	Matrix multiply(const Matrix& m) const {
-		if (!(line == m.line && collumn == m.collumn))
-			throw std::invalid_argument("Matrix dimension must be the same.");
-
-		Matrix result(line, collumn);
-
-		for (int i = 0; i < line; i++) {
-			for (int j = 0; j < collumn; j++) {
-				result.Matr[i][j] = Matr[i][j] * m.Matr[i][j];
-			}
-		}
-		return result;
-	}
-	//оператор присвивания
-	Matrix& operator = (const Matrix& a)
-	{
-		if (this->collumn > 0)
-		{
-			for (int i = 0; i < a.line; i++)
-				delete[] this->Matr[i];
-		}
-		if (this->line > 0)
-		{
-			delete[] this->Matr;
-		}
-		this->line = a.line;
-		this->collumn = a.collumn;
-		this->Matr = new long int* [a.line];
-		for (int i = 0; i < a.line; i++)
-			this->Matr[i] = new long int[a.collumn];
-		for (int i = 0; i < a.line; i++)
-		{
-			for (int j = 0; j < a.collumn; j++)
-				this->Matr[i][j] = a.Matr[i][j];
-		}
-		return *this;
-	}
-
-	//вывод через <<
-	friend std::ostream& operator << (std::ostream& out, Matrix& a) {
-		for (int i = 0; i < a.line; i++)
-		{
-			for (int j = 0; j < a.collumn; j++)
-			{
-				out.width(4);//промежуток между числами
-				out << a.Matr[i][j];
-			}
-			out << endl;
-		}
-		return out;
-	}
-	void summ_matrix(Matrix some)
-	{
-		for (int i = 0; i < min(line, some.line); i++)
-			for (int j = 0; j < min(collumn, some.collumn); j++)
-				Matr[i][j] += some.Matr[i][j];
-	}
-
-	
-	//Matrix& operator * (const Matrix& a)
-	//{
-	//	/*if (this->collumn != a.line)
-	//	{
-	//		throw exception("Matrices are incompatible");
-	//	}*/
-	//	Matrix result(this->line, a.collumn);
-
-	//	for (int i = 0; i < this->line; i++){
-	//		for (int j = 0; j < a.collumn; j++) {
-	//			for (int k = 0; k < this->collumn; k++) {
-	//				/*cout << this->Matr[i][k] << " ";
-	//				cout << a.Matr[k][j] << " ";*/
-	//				result.Matr[i][j] += this->Matr[i][k] * a.Matr[k][j];
-	//				/*result.Element(i, j) = (this->Matr[i][k] * a.Matr[k][j]);*/
-	//				cout << result;
-	//			}
-	//		}
-	//	}
-	//	return result;
-	//}
-	Matrix Matrix ::operator *(Matrix& b) {
-		Matrix c(this->line, b.collumn);
-		for (int k = 0; k < line; k++) {
-			for (int i = 0; i < collumn; i++)
-			{
-				c.Matr[k][i] = 0;
-				for (int j = 0; j < b.line; j++) {
-					c.Matr[k][i] += (Matr[k][j] * b.Matr[j][i]);
-				}
-			}
-		}
-		return c;
-	}
+	row(int a); // Конструктор
+	int* p;
+	int sz;
 };
 
+row::row(int a) : sz(a) // Конструктор
+{
+	p = new int[a];
+	if (!(p != 0)) {
+		throw exception("Память не выделилась");
+	}
+}
+
+int& row::operator [] (int j) // Оператор доступа к элементу
+{
+	if (!(j >= 0 && j < sz)) // Проверяется, не вышли ли мы за границы массива
+	{
+		throw exception("Выход за границы массива");
+	}
+	return p[j];
+}
+
+class matrix // Класс "матрица"
+{
+public:
+	matrix(int a, int b); // Конструктор, a-количество столбцов
+	matrix(const matrix& m); // Копирующий конструктор
+	~matrix(); // Деструктор
+	void init(); // Функция, инициализирующая массив значениями
+	void copy(row** x, int r, int c); // Функция копирования массива
+	row& operator [] (int i); // Оператор доступа к строке массива
+	matrix& operator = (const matrix& m); // Оператор присваивания
+	matrix operator + (const matrix& m); // Оператор сложения матриц
+	matrix operator * (const matrix& m); // Оператор умножения матриц
+	matrix operator * (int n); // Умножение матрицы на число
+	friend matrix operator * (int n, matrix& m); // Умножение числа на матрицу
+	friend ostream& operator << (ostream& out, const matrix& m); // Оператор вывода матрицы в поток
+private:
+	row** p; // Указатель на двумерный массив
+	int r; // Количество строк
+	row** create_mas(int a, int b); // Функция, выделяющая память под массив
+	void copyMassiv(row** x, int r, int c); // Функция, копирующая в новый массив элементы в нужном порядке 
+	void erase_mas(row** x, int r); // Функция, удаляющая массив
+};
+
+row** matrix::create_mas(int a, int b) // Функция, выделяющая память под массив
+{
+	row** q = new row * [a];
+	int i, j;
+	for (i = 0; i < a; i++)
+	{
+		q[i] = new row(b);
+		if (!(q[i] != 0)) // Проверяется, выделилась ли память
+		{
+			throw exception("Память не выделилась");
+		}
+	}
+	return q;
+}
+
+matrix::matrix(int a, int b) : r(a)  // Конструктор, a-количество строк, b-столбцов
+{
+	p = create_mas(a, b);
+}
+
+matrix::matrix(const matrix& m) : r(m.r) // Копирующий конструктор
+{
+	p = create_mas(m.r, m.p[0]->sz);
+	copy(m.p, m.r, m.p[0]->sz);
+}
+
+void matrix::copy(row** x, int r, int c) // Функция, копирующая массив
+{
+	int i, j;
+
+	for (i = 0; i < r; i++)
+		for (j = 0; j < c; j++)
+			p[i]->p[j] = x[i]->p[j];
+}
+
+void matrix::erase_mas(row** x, int r) // Функция, очищающая массив
+{
+	int i;
+	for (i = 0; i < r; i++)
+		delete[] x[i];
+	delete[] x;
+}
+
+matrix::~matrix() // Деструктор
+{
+	erase_mas(p, r); // Очищение массива
+}
+
+matrix& matrix::operator = (const matrix& m) // Оператор присваивания
+{
+	if (!(m.p[0]->sz == p[0]->sz && m.r == r))// Проверяется, равны ли размеры двух матриц
+	{
+		throw exception("Размеры матриц не равны");
+	}
+	int i, j;
+	copy(m.p, m.r, m.p[0]->sz);
+	return *this;
+}
+
+ostream& operator << (ostream& out, const matrix& a) // Оператор вывода матрицы в поток
+{
+	int i, j;
+	for (i = 0; i < a.r; i++)
+	{
+		for (j = 0; j < a.p[0]->get_sz(); j++)
+			out << a.p[i]->get_massiv()[j] << " ";
+		out << endl;
+	}
+	out << endl;
+	return out;
+}
+
+row& matrix::operator [] (int i) // Оператор доступа к строке матрицы
+{
+	if (!(i >= 0 && i < r)) // Проверяется, не вышли ли мы за границы массива
+	{
+		throw exception("Вы вышли за границы массива");
+	}
+	return *p[i];
+}
+
+matrix matrix::operator + (const matrix& m) // Оператор сложения двух матриц
+{
+	if (!(m.p[0]->sz == p[0]->sz && m.r == r)) // Проверяется, равны ли размеры двух матриц
+	{
+		throw exception("Размеры двух матриц не равны");
+	}
+	matrix n(m); // Создаётся новая матрица (копия 2ой)
+	int i, j;
+	for (i = 0; i < r; i++)
+		for (j = 0; j < p[0]->sz; j++)
+			n.p[i]->p[j] += p[i]->p[j]; // К скопированной матрице прибавляется 1ая
+	return n;
+}
+
+matrix matrix::operator * (const matrix& m) // Оператор умножения двух матриц
+{
+	if (!(p[0]->sz == m.r)) // Проверяется, равно ли количество столбцов 1ой матрицы количеству строк 2ой
+	{
+		throw exception("Количество столбцов 1ой матрицы не равно количеству строк 2ой");
+	}
+	matrix n(r, m.p[0]->sz); // Создаётся новая матрица необходимого размера
+	int i, j, k;
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < m.p[0]->sz; j++)
+		{
+			n.p[i]->p[j] = 0;
+			for (k = 0; k < p[0]->sz; k++)
+				n.p[i]->p[j] += p[i]->p[k] * m.p[k]->p[j]; // Результат умножения записывается в новую матрицу
+		}
+	}
+	return n;
+}
+
+matrix matrix::operator * (int n) { // Оператор умножения матрицы на число
+	matrix a(*this); // Создаётся копия матрицы 
+	int i, j;
+	for (i = 0; i < r; i++)
+		for (j = 0; j < p[0]->sz; j++)
+			a.p[i]->p[j] = p[i]->p[j] * n; // Скопированная матрица домножается на число
+	return a;
+}
+
+matrix operator * (int n, matrix& m) // Умножение числа на матрицу
+{
+	return m * n;
+}
+
+void matrix::copyMassiv(row** x, int r, int c) // Функция, копирующая в массив элементы в нужном порядке
+{
+	int i, j;
+	for (i = 0; i < c; i++)
+		for (j = 0; j < r; j++)
+			x[i]->p[j] = p[j]->p[i];
+}
+
+
+void matrix::init() // Функция, заполняющая матрицу случайными значениями
+{
+	srand(time(0));
+	int i, j;
+	for (i = 0; i < r; i++)
+		for (j = 0; j < p[0]->sz; j++)
+			p[i]->p[j] = rand() % 6; // Матрица заполняется значениями от 0 до 5
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,5 @@
+//Реализовал защищённый двумерный массив, перегрузил операторы обращения к элементу[][], сложения + , умножения * и
+// присваивания =. Вывод через cout, программа бросает исключения, матрица A заполняется случайными числами от 0 до 5 через функцию A.init()
 #include <iostream>
 #include "class.h"
 using namespace std;
@@ -5,65 +7,9 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-
-	//Vec a = Vec(10), b = Vec(10), c = Vec(10);
-
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	a.arr[i] = i;
-	//	b.arr[i] = i;
-	//}
-
-	//cout <<"Вектор 1: " <<a;
-	//cout <<"Вектор 2: " << b;
-	///*a.print();
-	//b.print();*/
-	//c = a + b;
-	///*c.print();*/
-	//cout <<"Сумма векторов: "<< c; //оператор переопределён
-	//
-
-	//
-	
-	int line=3;
-	/*cout << "Количество строк: "; cin >> line;*/
-	int collumn=3;
-	//cout << "Количество столбцов: "; cin >> collumn;
-	
-	Matrix M(line, collumn);
-
-	for (int i = 0; i < line; i++)
-	{
-		for (int j = 0; j < collumn; j++)
-			M.Element(i, j) = i+j;
-	}
-
-	cout << M;
-
-
-	Matrix L(line, collumn);
-	L = M;
-	L.MultiplyBy(5);
-
-	cout << "\n After multiply by 5\n" << endl;
-	cout << L;
-	
-
-	cout << L * M;
-
-	/*Matrix B(line, collumn);
-	M *= L;
-	cout << M;*/
-	//cout << "\n After summ\n" << endl;
-	//L.summ_matrix(M);
-	//cout << L;
-
-	/*cout << "\n After multiply \n" << endl;
-	M = M * L;
-	cout << M*L;
-	*/
-	return 0;
-	
-	
+	matrix a(3, 3);
+	a.init();
+	cout << a;
+	cout << a[0][0];
 
 }
